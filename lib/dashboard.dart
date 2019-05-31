b96de840abbbc151f24bd9a1ffb47f0ed122011e
Ryan Lewis <ryandlewis.rl@gmail.com>
2019-05-31 08:49:25 -0500

    Added basic connectivity checking

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'status.dart';
+import 'noconnection.dart';
 import 'package:connectivity/connectivity.dart';
 
 class Dashboard extends StatefulWidget {
@@ -14,31 +15,57 @@ class _DashboardState extends State<Dashboard> {
   List<GlobalKey<StatusState>> statusKeys =
       new List<GlobalKey<StatusState>>(machineNames.length);
   final String title;
-  DateTime updatedTime = DateTime.now();
+  String updatedTime;
+  ConnectivityResult connectivity = ConnectivityResult.none;
   _DashboardState(this.title);
 
   @override
+  void initState() {
+    super.initState();
+    _checkConnectivity();
+    updatedTime = _getTime();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(title),
+    if (connectivity == ConnectivityResult.none) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(title),
+          ),
+          actions: <Widget>[
+            new IconButton(
+                icon: const Icon(Icons.refresh), onPressed: _refreshStatus)
+          ],
         ),
-        actions: <Widget>[
-          new IconButton(
-              icon: const Icon(Icons.refresh), onPressed: _refreshStatus)
-        ],
-      ),
-      body: RefreshIndicator(
-        child: _machineList(),
-        onRefresh: _refreshStatus,
-      ),
-    );
+        body: RefreshIndicator(
+          child: NoConnection(),
+          onRefresh: _refreshStatus,
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(title),
+          ),
+          actions: <Widget>[
+            new IconButton(
+                icon: const Icon(Icons.refresh), onPressed: _refreshStatus)
+          ],
+        ),
+        body: RefreshIndicator(
+          child: _machineList(),
+          onRefresh: _refreshStatus,
+        ),
+      );
+    }
   }
 
   Widget _machineList() {
     return ListView.builder(
-        padding: const EdgeInsets.all(32.0),
+        padding: const EdgeInsets.all(10.0),
         itemBuilder: (context, i) {
           if (i < machineNames.length) {
             GlobalKey<StatusState> machineKey = new GlobalKey();
@@ -47,9 +74,8 @@ class _DashboardState extends State<Dashboard> {
           } else if (i == machineNames.length) {
             return Card(
               child: Container(
-                padding: EdgeInsets.all(10.0),
-                  child: Text(
-                      "Last Updated: ${updatedTime.hour}:${updatedTime.minute}:${updatedTime.second}")),
+                  padding: EdgeInsets.all(10.0),
+                  child: Text("Last Updated: ${updatedTime}")),
             );
           }
         });
@@ -58,8 +84,61 @@ class _DashboardState extends State<Dashboard> {
   Future<void> _refreshStatus() async {
 //    statusKeys.forEach((status) =>
 //        {if (status.currentState != null) status.currentState.updateStatus()});
+    var tempCon = await Connectivity().checkConnectivity();
     this.setState(() {
-      updatedTime = DateTime.now();
+      connectivity = tempCon;
+      updatedTime = _getTime();
     });
+  }
+
+  Future<void> _checkConnectivity() async {
+    var tempCon = await Connectivity().checkConnectivity();
+    setState(() {
+      connectivity = tempCon;
+    });
+  }
+
+  String _getTime() {
+    String month;
+    DateTime now = DateTime.now();
+    switch (now.month) {
+      case 1:
+        month = "January";
+        break;
+      case 2:
+        month = "February";
+        break;
+      case 3:
+        month = "March";
+        break;
+      case 4:
+        month = "April";
+        break;
+      case 5:
+        month = "May";
+        break;
+      case 6:
+        month = "June";
+        break;
+      case 7:
+        month = "July";
+        break;
+      case 8:
+        month = "August";
+        break;
+      case 9:
+        month = "September";
+        break;
+      case 10:
+        month = "October";
+        break;
+      case 11:
+        month = "November";
+        break;
+      case 12:
+        month = "December";
+        break;
+    }
+    return "$month ${now.day}, ${now.hour}:${(now.minute < 10) ? "0" + now.minute.toString() : now.minute}:${(now.second < 10) ? "0" + now.second.toString() : now.second}";
   }
 }
